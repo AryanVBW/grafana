d4487c49fba44dddacf52e61d272d87b0cd0e354
Yuri Tseretyan <yuriy.tseretyan@grafana.com>
2024-10-11 17:05:26 -0400

    fixup

--- pkg/apis/alerting_notifications/v0alpha1/types.go
+++ pkg/apis/alerting_notifications/v0alpha1/types.go
@@ -4,10 +4,7 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/grafana/grafana-app-sdk/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime/schema"
-	"k8s.io/apimachinery/pkg/types"
 )
 
 // region TimeInterval
@@ -281,104 +278,6 @@ func (o *RoutingTree) SetSpec(spec any) error {
 	}
 	o.Spec = cast
 	return nil
-}
-
-func (o *RoutingTree) GetStaticMetadata() resource.StaticMetadata {
-	gvk := o.GroupVersionKind()
-	return resource.StaticMetadata{
-		Name:      o.ObjectMeta.Name,
-		Namespace: o.ObjectMeta.Namespace,
-		Group:     gvk.Group,
-		Version:   gvk.Version,
-		Kind:      gvk.Kind,
-	}
-}
-
-func (o *RoutingTree) SetStaticMetadata(metadata resource.StaticMetadata) {
-	o.Name = metadata.Name
-	o.Namespace = metadata.Namespace
-	o.SetGroupVersionKind(schema.GroupVersionKind{
-		Group:   metadata.Group,
-		Version: metadata.Version,
-		Kind:    metadata.Kind,
-	})
-}
-
-func (o *RoutingTree) GetCommonMetadata() resource.CommonMetadata {
-	dt := o.DeletionTimestamp
-	var deletionTimestamp *time.Time
-	if dt != nil {
-		deletionTimestamp = &dt.Time
-	}
-	// Legacy ExtraFields support
-	extraFields := make(map[string]any)
-	if o.Annotations != nil {
-		extraFields["annotations"] = o.Annotations
-	}
-	if o.ManagedFields != nil {
-		extraFields["managedFields"] = o.ManagedFields
-	}
-	if o.OwnerReferences != nil {
-		extraFields["ownerReferences"] = o.OwnerReferences
-	}
-	return resource.CommonMetadata{
-		UID:               string(o.UID),
-		ResourceVersion:   o.ResourceVersion,
-		Generation:        o.Generation,
-		Labels:            o.Labels,
-		CreationTimestamp: o.CreationTimestamp.Time,
-		DeletionTimestamp: deletionTimestamp,
-		Finalizers:        o.Finalizers,
-		UpdateTimestamp:   o.GetUpdateTimestamp(),
-		CreatedBy:         o.GetCreatedBy(),
-		UpdatedBy:         o.GetUpdatedBy(),
-		ExtraFields:       extraFields,
-	}
-}
-
-func (o *RoutingTree) SetCommonMetadata(metadata resource.CommonMetadata) {
-	o.UID = types.UID(metadata.UID)
-	o.ResourceVersion = metadata.ResourceVersion
-	o.Generation = metadata.Generation
-	o.Labels = metadata.Labels
-	o.CreationTimestamp = metav1.NewTime(metadata.CreationTimestamp)
-	if metadata.DeletionTimestamp != nil {
-		dt := metav1.NewTime(*metadata.DeletionTimestamp)
-		o.DeletionTimestamp = &dt
-	} else {
-		o.DeletionTimestamp = nil
-	}
-	o.Finalizers = metadata.Finalizers
-	if o.Annotations == nil {
-		o.Annotations = make(map[string]string)
-	}
-	if !metadata.UpdateTimestamp.IsZero() {
-		o.SetUpdateTimestamp(metadata.UpdateTimestamp)
-	}
-	if metadata.CreatedBy != "" {
-		o.SetCreatedBy(metadata.CreatedBy)
-	}
-	if metadata.UpdatedBy != "" {
-		o.SetUpdatedBy(metadata.UpdatedBy)
-	}
-	// Legacy support for setting Annotations, ManagedFields, and OwnerReferences via ExtraFields
-	if metadata.ExtraFields != nil {
-		if annotations, ok := metadata.ExtraFields["annotations"]; ok {
-			if cast, ok := annotations.(map[string]string); ok {
-				o.Annotations = cast
-			}
-		}
-		if managedFields, ok := metadata.ExtraFields["managedFields"]; ok {
-			if cast, ok := managedFields.([]metav1.ManagedFieldsEntry); ok {
-				o.ManagedFields = cast
-			}
-		}
-		if ownerReferences, ok := metadata.ExtraFields["ownerReferences"]; ok {
-			if cast, ok := ownerReferences.([]metav1.OwnerReference); ok {
-				o.OwnerReferences = cast
-			}
-		}
-	}
 }
 
 func (o *RoutingTree) GetCreatedBy() string {
